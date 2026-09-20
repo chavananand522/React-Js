30d8a79db3a740602f51b0083e31798c6860cb51
chavananand522 <chavananand522@gmail.com>
2026-09-20 14:07:54 +0530

    Update React project

---
+++ router/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
